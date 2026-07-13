d937a09b13c13806dd34cfd7e3620b7b1b218048
Wz-z-4 <2710496170@qq.com>
2026-07-13 18:33:08 +0800

    第一版

---
+++ 1.py
@@ -0,0 +1,16 @@
+# this is for my training 
+class  student:
+    name = None
+    age = None 
+    nationality = None
+    subject = None
+stu_1= student()
+stu_1.age ="19"
+stu_1.name = "zz"
+stu_1.nationality = "China"
+stu_1.subject = "Math"
+print(stu_1.name)
+print(type(stu_1.age))
+print(stu_1.nationality)
+print(stu_1.subject)
+     
